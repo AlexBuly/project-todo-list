4e3f28c4717e88b235d3c6cfd50d43489515a1ba
Alex Buly <asbuly@icloud.com>
2024-06-28 21:13:22 -0400

    Adds initial pseudo code

---
+++ src/index.js
@@ -0,0 +1,20 @@
+console.log("Test");
+
+/*
+
+Modules called in index.js 
+
+At least 2 modules, todoItems/projects and DOMContent
+
+Main constructor/factory should have a title argument
+
+Within that, create additional todo-item constructor
+with title, description, and dueDate arguments 
+
+Update project name and todo-items 
+
+Delete projects/todo-items 
+
+Uses localStorage to save on computer 
+
+ */
