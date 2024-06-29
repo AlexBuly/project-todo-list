de19bced76656b5890be928ce7342caca44a81cb
Alex Buly <asbuly@icloud.com>
2024-06-28 20:53:17 -0400

    Adds html-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { Http2ServerRequest } = require('http2');
 
 module.exports = {
   entry: './src/index.js',
@@ -6,4 +8,12 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+        template: './src/index.html',
+        title: 'Todo List',
+        inject: 'head',
+        scriptLoading: 'defer',
+    }),
+  ]
 };
