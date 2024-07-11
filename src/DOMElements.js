1bc6ac3a3c298605a73e508f165b2ee464c5fddf
Alex Buly <asbuly@icloud.com>
2024-07-10 21:23:39 -0400

    Adds functions to create DOM elements

---
+++ src/DOMElements.js
@@ -0,0 +1,45 @@
+export function createForm(className) {
+    const form = document.createElement("form");
+    form.setAttribute("method", "post");
+    form.setAttribute("action", "/");
+    if (className) form.classList.add(className);
+    return form;
+}
+
+export function createInput(type, name, id) {
+    const input = document.createElement("input");
+    input.type = type;
+    if (name) input.name = name;
+    if (id) input.id = id;
+    return input;
+}
+
+export function createFieldset() {
+    const fieldset = document.createElement("fieldset");
+    return fieldset;
+}
+
+export function createLabel(htmlFor, textContent) {
+    const label = document.createElement("label");
+    label.htmlFor = htmlFor;
+    label.textContent = textContent;
+    return label;
+}
+
+export function createButton(textContent, className) {
+    const button = document.createElement("button");
+    button.textContent = textContent;
+    if (className) button.className = className;
+    return button;
+}
+
+export function createDiv(className) {
+    const div = document.createElement("div");
+    if (className) div.className = className;
+    return div;
+}
+
+export function createBreak() {
+    const br = document.createElement("br");
+    return br;
+}
